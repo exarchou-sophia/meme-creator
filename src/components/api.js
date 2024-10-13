db1674b7e7360eea5579081225386c6ab46d2547
Sofia Ex <dev.exarchou@outlook.com>
2024-10-13 17:34:18 +0200

    get memes from nested result

--- src/components/api.js
+++ src/components/api.js
@@ -1,2 +1,4 @@
 export const getMemes = () =>
-    fetch("https://api.imgflip.com/get_memes").then(res => res.json());
+    fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(res => res.data.memes);
