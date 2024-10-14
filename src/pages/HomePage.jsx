34523b580ff98cf8a862bcea0dffc5adbda7069b
Sofia Ex <dev.exarchou@outlook.com>
2024-10-15 00:00:36 +0200

    filter by user's input and remove button

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,7 +9,6 @@ export const HomePage = () => {
         setMemes,
         setPreviewMeme,
     } = useMemes();
-    const [search, setSearch] = useState("");
     const [filteredMemes, setFilteredMemes] = useState([]);
 
     useEffect(() => {
@@ -28,29 +27,19 @@ export const HomePage = () => {
                 <MemePreview />
             </main>
 
-            <aside
-
-                className="flex items-start w-1/3 flex-col px-4 py-4 ml-auto my-2h-auto "
-            >
+            <aside className="flex items-start w-1/3 flex-col px-4 py-4 ml-auto my-2h-auto ">
                 <div className="flex bg-white  px-1 py-1 rounded-lg border border-indigo-500 
                 overflow-hidden max-w-md mx-auto font-[sans-serif]">
                     <input
                         type='email'
                         placeholder='Search for a picture...'
                         className="w-full outline-none bg-white pl-4 text-sm"
-                        onChange={({ target }) =>
-                            setSearch(target.value)
-                        }
+                        onChange={({ target }) => setFilteredMemes(
+                            target.value === ""
+                                ? []
+                                : filterMemes(target.value)
+                        )}
                     />
-
-                    <button
-                        type='button'
-                        className="bg-indigo-500 hover:bg-amber-600  transition-all text-white 
-                        text-sm rounded-lg px-5 py-2.5"
-                        onClick={() => setFilteredMemes(filterMemes(search))}
-                    >
-                        Search
-                    </button>
                 </div>
 
                 <div>
@@ -62,9 +51,7 @@ export const HomePage = () => {
                              overflow-hidden transition-transform transform hover:scale-95 hover:shadow-md "
                         >
                             <img src={meme.url} className="w-auto rounded-sm m-3 px-3" />
-
                             <p>{meme.name}</p>
-
                         </div>
                     ))}
                 </div>
