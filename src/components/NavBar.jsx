1399a6e891d6ebb5fcee70878b3e882331a888c2
Sofia Ex <dev.exarchou@outlook.com>
2024-10-15 21:47:15 +0200

    nav styles fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -18,9 +18,9 @@ export const NavBar = () => (
         )}
     >
         <Link to="/" className="mr-4 text-lg text-white">
-            <img src='./logo-removebg.png' className='w-2/3 h-16 object-cover' />
+            <img src='./logo-removebg.png' className='w-1/2 h-12 object-cover' />
         </Link>
-        <Link to="/gallery" className="text-xl m-4 hover:text-amber-300 text-lg text-white transition-colors duration-300text-white">
+        <Link to="/gallery" className="text-xl m-2 hover:text-amber-300 text-lg text-white transition-colors duration-300text-white">
             Gallery
         </Link>
     </nav>
